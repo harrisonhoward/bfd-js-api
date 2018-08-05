ed99f1b562f246749674ac54c7f5b5aee6907737
Forbidden-Duck <forbiddenducky@gmail.com>
2018-08-06 05:50:14 +1000

    v1.3.11 - Fixed up alignment on Avatar & all 3 right boxes

--- src/EmbedMaker.js
+++ src/EmbedMaker.js
@@ -15,10 +15,10 @@ class AutoPost {
                 .setShadowColor("rgba(22, 22, 22, 1)")
                 .setShadowOffsetY(5)
                 .setShadowBlur(10)
-                .addCircle(105, 157.5, 62)
-                .addRect(205, 92, 250, 30)
-                .addRect(205, 142, 250, 30)
-                .addRect(205, 192, 250, 30)
+                .addCircle(105, 152.5, 62)
+                .addRect(205, 87, 250, 30)
+                .addRect(205, 147, 250, 30)
+                .addRect(205, 187, 250, 30)
                 .addRect(100, 237, 355, 30)
                 .setColor('#7289DA')
                 .addRect(0, 0, 500, 40)
@@ -40,7 +40,7 @@ class AutoPost {
                 .addText(botCount, 442, 213)
                 .addText(bot.owner, 442, 259)
                 .restore()
-                .addRoundImage(avatar, 40, 96, 128, 128, 64)
+                .addRoundImage(avatar, 40, 91, 128, 128, 64)
                 .toBuffer();
         } catch (err) {
             console.error(err);
